ccfbb7db23bba8a2b85ebbd7383f407bfdeba94d
nagendra duvvapunaka <nagendra.d522@gmail.com>
2020-10-15 16:48:02 +0530

    intial check-in

---
+++ ContactsWebAPI/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactsWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private List<ContactModel> ContactList;
+
+
+        public ContactController()
+        {
+            ContactList = new List<ContactModel>();
+        }
+
+
+        
+        [HttpPost]
+        [Route("CreateorUpdateContact")]
+        public async Task<IActionResult> Post([FromBody] ContactModel contact)
+        {
+            if (contact == null)
+                return BadRequest(contact);
+
+            var existedcontact = ContactList.Find(x => x.FirstName == contact.FirstName && x.LastName == contact.LastName);
+
+            if (existedcontact != null)
+                ContactList[ContactList.IndexOf(existedcontact)] = contact;
+            else
+                ContactList.Add(contact);
+
+            return Ok(ContactList);
+        }
+
+    }
+}
---
+++ ContactsWebAPI/Models/ContactModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsWebAPI.Models
+{
+    public class ContactModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public List<string>  EmaildIds { get; set; }
+        public List<string> PhoneNumbers { get; set; }
+    }
+}
